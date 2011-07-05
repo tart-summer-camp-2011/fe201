a38fd66ff473c9e0eae7840712a2d46ab352f908
dashersw <armagan.amcalar@tart.com.tr>
2011-07-05 10:22:24 +0300

    implemented the Tweet class.

--- twitter/js/lib/Tweet.js
+++ twitter/js/lib/Tweet.js
@@ -2,3 +2,10 @@
 // Techinox Commercial License
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
+
+var Tweet = function(user, body){
+    this.id = tart.getUId();
+    this.user = user;
+    this.body = body;
+    this.time = tart.randomTime();
+}
