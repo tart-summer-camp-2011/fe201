d84318884f071a5d70bc98d52df46c6a769b0324
dashersw <armagan.amcalar@tart.com.tr>
2011-07-05 10:22:48 +0300

    implemented the TweetModel class.

--- twitter/js/models/TweetModel.js
+++ twitter/js/models/TweetModel.js
@@ -2,3 +2,12 @@
 // Techinox Commercial License
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
+
+var TweetModel = function() {
+
+};
+
+TweetModel.prototype.postTweet = function(user, body) {
+    var tweet = new Tweet(user, body);
+    localStorage.setObject(tweet.id, tweet);
+}
